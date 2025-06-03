3cace55956e0c53d0a9e9cace6b476a9118e3d69
rayden <raydenldon@gmail.com>
2025-06-03 13:20:36 +0200

    Prueba 2

--- core/src/main/java/io/github/EarthDigger/EarthDigger.java
+++ core/src/main/java/io/github/EarthDigger/EarthDigger.java
@@ -1,34 +1,96 @@
 package io.github.EarthDigger;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.*;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class EarthDigger extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+//public class EarthDigger extends ApplicationAdapter implements ApplicationListener {
+public class EarthDigger extends InputAdapter implements ApplicationListener {
+    private float deltaTime = 0;
+    private float distance = 0;
+    OrthographicCamera camera;
+    SpriteBatch batch;
+    Sprite sprite;
+
+
+    @Override
+    public boolean keyDown(int keycode) {
+        switch (keycode){
+            case Input.Keys.SPACE:
+                System.out.println("SPACE");
+                break;
+            case Input.Keys.P:
+                System.out.println("P");
+                break;
+            case Input.Keys.A:
+                System.out.println("A");
+                break;
+            case Input.Keys.D:
+                System.out.println("D");
+                break;
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        System.out.println("CLICK: " + screenX + ", " + screenY);
+        return true;
+    }
 
     @Override
     public void create() {
+        Gdx.input.setInputProcessor(this);
+        camera = new OrthographicCamera(700, 400);
         batch = new SpriteBatch();
-        image = new Texture("piña2.png");
+        sprite = new Sprite(new Texture("pescao.png"));
+        sprite.setPosition(-350, -200);
+        sprite.setSize(100, 100);
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        deltaTime = Gdx.graphics.getDeltaTime();
+
+        distance = distance + (20*deltaTime);
+
+
+        batch.setProjectionMatrix(camera.combined);
+
         batch.begin();
-        batch.draw(image, 140, 210);
+        sprite.draw(batch);
         batch.end();
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        image.dispose();
+        sprite.getTexture().dispose();
     }
+
+    @Override
+    public void resize (int width, int height){
+
+
+    }
+
 }
