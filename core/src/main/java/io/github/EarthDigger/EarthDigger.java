e8f601cd53aaf16fb549db81562d7fef0ff4e147
StoianAlejandro <s.alejandrostoian@gmail.com>
2025-06-03 20:32:01 +0200

    La pantalla se queda quieta cuando llega a los bordes

--- core/src/main/java/io/github/EarthDigger/EarthDigger.java
+++ core/src/main/java/io/github/EarthDigger/EarthDigger.java
@@ -1,6 +1,7 @@
 package io.github.EarthDigger;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.*;
@@ -12,9 +13,9 @@ import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
-public class EarthDigger extends ApplicationAdapter {
-    int screenSizeX = 50;
-    int screensizeY = 20;
+public class EarthDigger extends ApplicationAdapter implements ApplicationListener {
+    int screenSizeX = 100;
+    int screensizeY = 100;
     Texture backgroundTexture;
     Texture pinyaTexture;
     SpriteBatch spriteBatch;
@@ -27,40 +28,61 @@ public class EarthDigger extends ApplicationAdapter {
     public void create () {
         backgroundTexture = new Texture("frutas.jpg");
         background = new Sprite(backgroundTexture);
-        background.setSize(100,90);
-        background.setPosition(-20, -20);
+        background.setSize(400,100);
+        background.setPosition(0, -20);
         pinyaTexture = new Texture("images.jpg");
         pinya = new Sprite(pinyaTexture);
         pinya.setSize(3,5);
         camera = new OrthographicCamera();
-        viewport = new ExtendViewport(70, 50, camera);
-        camera.position.x=pinya.getX()+ pinya.getHeight()/2f;
-        camera.position.y=pinya.getY()+ pinya.getWidth()/2f;
-        camera.update();
+        viewport = new ExtendViewport(screenSizeX, screensizeY, camera);
         spriteBatch = new SpriteBatch();
     }
 
     @Override
     public void render () {
-        ScreenUtils.clear(Color.BLACK);
-
-        viewport.apply();
+        draw();
         if (Gdx.input.isKeyPressed(Input.Keys.A)) {
             pinya.setX(pinya.getX() - (float)0.5);
         }
         if (Gdx.input.isKeyPressed(Input.Keys.D)) {
             pinya.setX(pinya.getX() + (float)0.5);
         }
-        camera.position.x=pinya.getX()+ pinya.getHeight()/2f;
-        camera.position.y=pinya.getY()+pinya.getWidth()/2f;
+
+    }
+
+    public void draw() {
+        ScreenUtils.clear(Color.BLACK);
+        viewport.apply();
+
+        // POSICION DE CÁMARA
+        camera.position.x = pinya.getX() + pinya.getWidth() / 2f;
+        camera.position.y = pinya.getY() + pinya.getHeight() / 2f + 30f;
+
+        // POSICION DE CÁMARA FIJA CUANDO LLEGA AL BORDE DE LA PANTALLA
+        if (camera.position.x < viewport.getWorldWidth() / 2f) {
+            camera.position.x = viewport.getWorldWidth() / 2f;
+        }
+        if (camera.position.x > background.getX() + background.getWidth() - viewport.getWorldWidth() / 2f) {
+            camera.position.x = background.getX() + background.getWidth() - viewport.getWorldWidth() / 2f;
+        }
         camera.update();
+
         spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
 
-
+        //CARGAR SPRITES
         spriteBatch.begin();
         background.draw(spriteBatch);
         pinya.draw(spriteBatch);
         spriteBatch.end();
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
 
     }
 
