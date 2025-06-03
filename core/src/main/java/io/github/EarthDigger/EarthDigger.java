f2c6ef603dba14ba45cad53a39ca888a6567dda6
StoianAlejandro <s.alejandrostoian@gmail.com>
2025-06-03 19:26:01 +0200

    CÃámara persigue al jugador y el fondo permanece estático

--- core/src/main/java/io/github/EarthDigger/EarthDigger.java
+++ core/src/main/java/io/github/EarthDigger/EarthDigger.java
@@ -8,43 +8,58 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class EarthDigger extends ApplicationAdapter {
     int screenSizeX = 50;
-    int screensizeY = 10;
+    int screensizeY = 20;
+    Texture backgroundTexture;
     Texture pinyaTexture;
     SpriteBatch spriteBatch;
     Viewport viewport;
-    Camera camera;
+    OrthographicCamera camera;
     Sprite pinya;
+    Sprite background;
 
     @Override
     public void create () {
+        backgroundTexture = new Texture("frutas.jpg");
+        background = new Sprite(backgroundTexture);
+        background.setSize(100,90);
+        background.setPosition(-20, -20);
         pinyaTexture = new Texture("images.jpg");
         pinya = new Sprite(pinyaTexture);
         pinya.setSize(3,5);
         camera = new OrthographicCamera();
-        viewport = new FitViewport(screenSizeX, screensizeY, camera);
-        camera.position.x=screenSizeX/2f;
-        camera.position.y=screensizeY/2f;
+        viewport = new ExtendViewport(70, 50, camera);
+        camera.position.x=pinya.getX()+ pinya.getHeight()/2f;
+        camera.position.y=pinya.getY()+ pinya.getWidth()/2f;
+        camera.update();
         spriteBatch = new SpriteBatch();
     }
 
     @Override
     public void render () {
         ScreenUtils.clear(Color.BLACK);
+
         viewport.apply();
-        spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
-        spriteBatch.begin();
-        pinya.draw(spriteBatch);
         if (Gdx.input.isKeyPressed(Input.Keys.A)) {
             pinya.setX(pinya.getX() - (float)0.5);
         }
         if (Gdx.input.isKeyPressed(Input.Keys.D)) {
             pinya.setX(pinya.getX() + (float)0.5);
         }
+        camera.position.x=pinya.getX()+ pinya.getHeight()/2f;
+        camera.position.y=pinya.getY()+pinya.getWidth()/2f;
+        camera.update();
+        spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
+
+
+        spriteBatch.begin();
+        background.draw(spriteBatch);
+        pinya.draw(spriteBatch);
         spriteBatch.end();
 
     }
@@ -52,6 +67,7 @@ public class EarthDigger extends ApplicationAdapter {
     @Override
     public void resize(int width, int height) {
         viewport.update(width, height);
+        camera.update();
     }
 
     @Override
