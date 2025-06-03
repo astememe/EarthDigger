bdb31d167e9838e8e1829b752be9ba78cfd697c1
StoianAlejandro <s.alejandrostoian@gmail.com>
2025-06-03 13:18:35 +0200

    Conseguido movimiento lateral con poiña

--- core/src/main/java/io/github/EarthDigger/EarthDigger.java
+++ core/src/main/java/io/github/EarthDigger/EarthDigger.java
@@ -2,33 +2,60 @@ package io.github.EarthDigger;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class EarthDigger extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+    int screenSizeX = 50;
+    int screensizeY = 10;
+    Texture pinyaTexture;
+    SpriteBatch spriteBatch;
+    Viewport viewport;
+    Camera camera;
+    Sprite pinya;
 
     @Override
-    public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("piña2.png");
+    public void create () {
+        pinyaTexture = new Texture("images.jpg");
+        pinya = new Sprite(pinyaTexture);
+        pinya.setSize(3,5);
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(screenSizeX, screensizeY, camera);
+        camera.position.x=screenSizeX/2f;
+        camera.position.y=screensizeY/2f;
+        spriteBatch = new SpriteBatch();
     }
 
     @Override
-    public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+    public void render () {
+        ScreenUtils.clear(Color.BLACK);
+        viewport.apply();
+        spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
+        spriteBatch.begin();
+        pinya.draw(spriteBatch);
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+            pinya.setX(pinya.getX() - (float)0.5);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+            pinya.setX(pinya.getX() + (float)0.5);
+        }
+        spriteBatch.end();
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        image.dispose();
+        spriteBatch.dispose();
     }
 }
