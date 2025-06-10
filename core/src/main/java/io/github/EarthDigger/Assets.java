8f7013a41a7fae316947dfac35c9f0ef544d4745
StoianAlejandro <s.alejandrostoian@gmail.com>
2025-06-10 16:28:09 +0200

    APara crear el indicador de vida

--- core/src/main/java/io/github/EarthDigger/Assets.java
+++ core/src/main/java/io/github/EarthDigger/Assets.java
@@ -7,11 +7,13 @@ public class Assets {
     public static Texture cespedTexture;
     public static Texture tierraTexture;
     public static Texture piedraTexture;
+    public static Texture vidaTexture;
 
     public static void load() {
         cespedTexture = new Texture(Gdx.files.internal("cespedfinal.png"));
         tierraTexture = new Texture(Gdx.files.internal("bloquetierra.png"));
         piedraTexture = new Texture(Gdx.files.internal("piedraFINAL.png"));
+        vidaTexture = new Texture(Gdx.files.internal("corazonTexture.png"));
     }
 
     public static void dispose() {
